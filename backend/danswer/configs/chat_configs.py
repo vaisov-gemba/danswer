8fb8724b159c023379ecae24465a7f5d19bf553a
pablodanswer <pablo@danswer.ai>
2024-10-29 10:47:02 -0700

    add constant

--- backend/danswer/configs/chat_configs.py
+++ backend/danswer/configs/chat_configs.py
@@ -69,6 +69,9 @@ LANGUAGE_CHAT_NAMING_HINT = (
     or "The name of the conversation must be in the same language as the user query."
 )
 
+# Number of prompts each persona should have
+NUM_PERSONA_PROMPTS = 4
+
 # Agentic search takes significantly more tokens and therefore has much higher cost.
 # This configuration allows users to get a search-only experience with instant results
 # and no involvement from the LLM.
